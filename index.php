848f1b0191d36d94b1187e524707ff6487e50451
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2023-05-22 20:55:44 +0300

    logo si meniu sus

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="icon" href="poze/logo_ureche_numai_cerc.png">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,9 +11,9 @@
 <body>
     <nav class="nav-bar">  
         <ul class="nav-ul">
-            <li class="nav-ul-li"><a class="nav-a" href="#">Home</a></li>
-            <li class="nav-ul-li"><a class="nav-a" href="#">Next</a></li>
-            <li class="nav-ul-li"><a class="nav-a" href="#">More</a></li>
+            <a class="nav-a" href="#">Home</a>
+            <a class="nav-a" href="#">Next</a>
+            <a class="nav-a" href="#">More</a>
         </ul>
     </nav>
 </body>
--- style.php
+++ style.php
@@ -4,11 +4,18 @@
 .nav-bar
 {
     background-color: black;
+    
 }
 .nav-a
 {
     color: white;
     font-weight: bold;
-    padding: 5%;
-    
+    margin: 5 5px;
+    display: inline-block;
+    font-size: 120%;
+    padding: 0.5% 0.9%;
+    border-radius: 3px;
+    text-transform: uppercase;
+    transform: translate(0%, 0%);
+    background: black;
 }
