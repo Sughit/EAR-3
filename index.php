a5fe5ccd0b56f81ad15574c9e1d0c3b1a349b2ad
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2023-05-22 22:49:52 +0300

    meniu stanga

--- index.php
+++ index.php
@@ -8,17 +8,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="style.php"/>
+    <link rel="stylesheet" type="text/css" href="style.php">
     <title>EAR-3 Market</title>
 </head>
 <body class="body">
     <nav class="nav-bar">  
         <ul class="nav-ul">
-            <a class="nav-a" href="#">Home</a>
-            <a class="nav-a" href="#">Next</a>
-            <a class="nav-a" href="#">More</a>
+            <br><br>
         </ul>
     </nav>
+    
+
+
+    <div id="main">
+        <span id="open">&#9776; Menu<span>
+    </div>
+    <div id ="mySidenav" class="sidenav">
+        <div class="box-c1">
+            <a href="#" class="logo"><img src="poze/logo_ureche_numai_cerc.png" width=45%></a>
+            <a href="#" class="closebtn" id="close"> &times;</a>          
+        </div>
+
+        <ul class ="nav-list">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+</div>
+    <script>
+        const openNav =document.getElementById("open");
+        const closeNav =document.getElementById("close");
+        const navSlide =document.getElementById("mySidenav");
+
+        openNav.addEventListener("click", ()=>{
+            navSlide.classList.add("navside");
+        });
+
+        closeNav.addEventListener("click", ()=>{
+            navSlide.classList.remove("navside");
+        });
+
+    </script>
+
+
     <footer>
         <p align="center">
         <a class="nav-a">English</a> | <a class="nav-a">Romanian</a>
--- style.php
+++ style.php
@@ -6,7 +6,7 @@
     background-color: #31313D;
     scrollbar-width: none;
 }
-::-webkit-scrollbar /*scrollbar pentru chrome*/
+::-webkit-scrollbar
 {
     width: 0;
 }
@@ -29,6 +29,96 @@
     padding: 0.5% 0.9%;
     border-radius: 3px;
     text-transform: uppercase;
-    transform: translate(0%, 0%);
+    transform: translate(20%, 0%);
     background: black;
+}
+#main
+{
+    display: flex;
+    position: absolute;
+    top: 1%;
+    left: 5%;
+    scale: 60%;
+}
+
+#main span
+{
+   font-size: 30px;
+   background-color: #000;
+   color: white;
+   padding: 4px 10px;;
+   border-radius: 5px;
+   cursor: pointer;
+}
+.sidenav
+{
+   background-color: #212129;
+   width:0;
+   height: 100vh;
+   position: relative;
+   z-index: 1;
+   top: 0;
+   left: 0; 
+   transition: all 0.5s;
+   padding-top: 30px;
+   opacity: 0;
+   visibility: hidden;
+}
+.sidenav .box-c1
+{
+    display: flex;
+    flex-direction: row;
+    padding: 3px 15px;
+    position: relative;
+}
+
+.sidenav .box-c1 a.logo
+{
+    font-size: 35px;
+    opacity:1;
+    color: #000000;
+    text-decoration: none;
+    font-weight: 600;
+    text-transform: uppercase;
+}
+
+.sidenav .closebtn
+{
+    position: absolute;
+    top: 0;
+    right: -35px;
+    font-size: 36px;
+    padding: 0px 7px;
+    color: #fff;
+    text-decoration:none;
+    transition: all 0.02s linear;
+}
+.sidenav .nav-list
+{
+    padding: 0;
+    list-style: none;
+}
+
+.sidenav .nav-list a
+{
+    padding: 10px 8px 8px 32px;
+    margin: 10px;
+    text-decoration: none;
+    font-size:25px;
+    font-weight:500;
+    color:#000;
+    display: block;
+    background-color: #b8b8ba;
+    transition: all 0.1s;
+}
+.sidenav .nav-list a:hover
+{
+    background-color: #363647;
+    color:#fff;
+}
+.navside
+{
+    width: 300px;
+    visibility: visible;
+    opacity:1;
 }
