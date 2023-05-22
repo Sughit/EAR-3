bf63ac79e35b507e9737b56b96c0deea7ff6a350
Sughit <andreisorici2007@gmail.com>
2023-05-22 19:43:49 +0300

    pre-alpha 1.0.0

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.php"/>
+    <title>EAR-3</title>
+</head>
+<body>
+    <nav class="nav-bar">  
+        <ul class="nav-ul">
+            <li class="nav-ul-li"><a class="nav-a" href="#">Home</a></li>
+            <li class="nav-ul-li"><a class="nav-a" href="#">Next</a></li>
+            <li class="nav-ul-li"><a class="nav-a" href="#">More</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
+<?php
+
+?>
---
+++ style.php
@@ -0,0 +1,14 @@
+<?php
+    header("Content-type: text/css; charset: UTF-8");
+?>
+.nav-bar
+{
+    background-color: black;
+}
+.nav-a
+{
+    color: white;
+    font-weight: bold;
+    padding: 5%;
+    
+}
