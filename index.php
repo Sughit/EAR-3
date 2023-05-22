d3e0393d9172f490f4e02500baa106480c00dceb
Sughit <andreisorici2007@gmail.com>
2023-05-22 20:55:07 +0300

    inceputul pentru internationalitate

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    include "config.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,18 +8,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.php"/>
-    <title>EAR-3</title>
+    <title>EAR-3 Market</title>
 </head>
-<body>
+<body class="body">
     <nav class="nav-bar">  
         <ul class="nav-ul">
             <li class="nav-ul-li"><a class="nav-a" href="#">Home</a></li>
-            <li class="nav-ul-li"><a class="nav-a" href="#">Next</a></li>
-            <li class="nav-ul-li"><a class="nav-a" href="#">More</a></li>
+            <li class="nav-ul-li"><a class="nav-a" href="#">Next</a></li>                <li class="nav-ul-li"><a class="nav-a" href="#">More</a></li>
         </ul>
     </nav>
+    <footer>
+        <p align="center">
+        <a class="nav-a">English</a> | <a class="nav-a">Romanian</a>
+        </p>
+    </footer>
 </body>
 </html>
-<?php
-
-?>
--- style.php
+++ style.php
@@ -1,14 +1,21 @@
 <?php
     header("Content-type: text/css; charset: UTF-8");
 ?>
+.body
+{
+    background-color: #31313D;
+}
 .nav-bar
 {
     background-color: black;
+}
+.nav-ul
+{
+    position: right;
 }
 .nav-a
 {
     color: white;
     font-weight: bold;
     padding: 5%;
-    
 }
