04d26f726261e9c8a08eb2ccae542e19d483b234
emanuel5867 <130173546+emanuel5867@users.noreply.github.com>
2023-05-23 11:44:19 +0300

    mici schimbari

--- limbi/lang-ro.php
+++ limbi/lang-ro.php
@@ -12,7 +12,7 @@
         "news" => "Stiri",
         "menu" =>"Meniu",
         "textHome"=> "3 oameni extrem de eleganti si rafinati",
-        "textStore" => "Aici probabil si aproape sigur vom vinde chestii pentru toti si tote persoanele din lume. Oriunde te afli, va ajunge produsul la tine",
+        "textStore" => "Aici probabil si aproape sigur vom vinde chestii pentru toti si toate persoanele din lume. Oriunde te afli, va ajunge produsul la tine",
         "textCommunity"=> "Aici este comunitatea noastra extrem de numeroasa care cu siguranta exista",
         "textAbout"=> "Aici este ceva numit 'despre'. poate aflati mai multe despre noi, mai stii?",
         "textSupport"=> "Ajutor. Nu, serios. am nevoide de ajutor."
